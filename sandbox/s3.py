b7a1c9d8739b513cff33138e1b1cb5bd24494e2a
shukubota <kubotashu0421@gmail.com>
2022-12-25 14:13:22 +0900

    s3のxlsxを読む

--- sandbox/s3.py
+++ sandbox/s3.py
@@ -1,12 +1,22 @@
 import boto3
 from botocore.config import Config
+import pandas as pd
+import io
 
 AWS_PROFILE = 'localstack'
 AWS_REGION = 'ap-northeast-1'
 ENDPOINT = 'http://localhost:4566'
 
 boto3.setup_default_session(profile_name=AWS_PROFILE)
-s3 = boto3.resource('s3', region_name=AWS_REGION, endpoint_url=ENDPOINT)
+s3 = boto3.client('s3', region_name=AWS_REGION, endpoint_url=ENDPOINT)
 
-for bucket in s3.buckets.all():
-        print(bucket.name)
+response = s3.get_object(Bucket="s3-test", Key="株式会社auditcheck_test.xlsx")
+# print(response['Body'].read())
+content = response['Body'].read()
+
+# Filepath = os.path.abspath('mapping.xlsx') 
+df = pd.read_excel(io.BytesIO(content), sheet_name='シート1')  
+print(df)
+
+# for bucket in s3.buckets.all():
+#         print(bucket.name)
