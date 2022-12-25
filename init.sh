6a89643ebf1739b7e91787e8e072b8bc26d141cc
shukubota <kubotashu0421@gmail.com>
2022-12-25 13:52:01 +0900

    s3 uploadコマンド

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 aws s3api create-bucket --bucket s3-test --endpoint-url=http://localhost:4566 \
 --create-bucket-configuration LocationConstraint=ap-northeast-1
+
+aws s3 cp ./株式会社auditcheck_test.xlsx s3://s3-test --profile localstack --endpoint-url http://localhost:4566
