1debd004d80c00618140af7af2a7630b646e2eba
shukubota <kubotashu0421@gmail.com>
2023-03-12 22:51:50 +0900

    chatcompletionの例

--- chat-gpt/chat_gpt.py
+++ chat-gpt/chat_gpt.py
@@ -4,3 +4,12 @@ import openai
 openai.api_key = os.getenv('CHAT_GPT_API_KEY')
 model_list = openai.Model.list()
 print(model_list)
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": "大谷翔平について教えて"},
+    ],
+)
+
+print(response)
